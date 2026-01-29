6c3639fcf5d4b5595664d1a1344c0c9e9086d078
IanArcadeBooth <w0513690@casmpus.nscc.ca>
2026-01-29 18:10:42 -0400

    1

---
+++ keyled/keyled.c
@@ -0,0 +1,13 @@
+#include <linux/kd.h> //KDSETLED
+#include <sys/ioctl.h>
+#include <unistd.h> //sleep()
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+	int num[] = {0,1,4,5,2,3,6,7}; //this array holds the correct order that the LEDs on the keyboard will light up
+	int key; //holds the user input
+
+	scanf("%d", &key); //uses scanf and stores entry in key in variable
+	ioctl(1,KDSETLED, num[key]); //lights up the corresponding LEDS based on the user input key and the spot in the array num
+}
